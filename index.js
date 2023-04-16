5c8cccaf295082c1b47417a35a371e4d81033f2d
Dorsey B <dorseybec@gmail.com>
2023-04-15 23:50:36 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+var employee = {
+    name: "",
+    streetAddress : ""
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    /* This function should not mutate the employee; 
+    it should return a new Object that has an updated value for the key passed in.
+     Hint: use the spread operator! */
+    let obj = {...employee};
+    obj[key]  = value;
+    return obj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]  = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    /* This should not mutate the original employee Object; 
+    it should return a new Object that doesn't include the identified key-value pair. 
+    Hint: use the spread operator!*/
+    let obj = {...employee};
+    delete obj[key];
+    return obj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
